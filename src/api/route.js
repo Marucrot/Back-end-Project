b1e9421f8beace4483f560313c113b3b05808d4c
dominikusdelfino-dotcom <dominikus.delfino@gmail.com>
2026-04-13 22:27:13 +0700

    payment

--- src/api/route.js
+++ src/api/route.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const users = require('./components/users/userRoute');
+const payment = require('./components/payment/paymentRoute');
 
 module.exports = () => {
   const app = express.Router();
 
   users(app);
+  payment(app);
 
   return app;
 };
