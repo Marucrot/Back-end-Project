00017da2007c696671a0ed0f368dcef4525887db
dominikusdelfino-dotcom <dominikus.delfino@gmail.com>
2026-04-22 22:00:15 +0700

    hello!!

--- src/api/components/payment/paymentController.js
+++ src/api/components/payment/paymentController.js
@@ -1,1 +1,54 @@
-tes
+const paymentService = require('./paymentService');
+
+async function createPayment(req, res, next) {
+  try {
+    const result = await paymentService.createPayment(req.body);
+    return res.status(201).json(result);
+  } catch (error) {
+    return next(error);
+  }
+}
+
+async function checkoutPayment(req, res, next) {
+  try {
+    const result = await paymentService.checkoutPayment(req.body);
+    return res.status(200).json(result);
+  } catch (error) {
+    return next(error);
+  }
+}
+
+async function checkStatus(req, res, next) {
+  try {
+    const result = await paymentService.checkStatus(req.body);
+    return res.status(200).json(result);
+  } catch (error) {
+    return next(error);
+  }
+}
+
+async function cancelPayment(req, res, next) {
+  try {
+    const result = await paymentService.cancelPayment(req.body);
+    return res.status(200).json(result);
+  } catch (error) {
+    return next(error);
+  }
+}
+
+async function paymentHistory(req, res, next) {
+  try {
+    const result = await paymentService.paymentHistory(req.body);
+    return res.status(200).json(result);
+  } catch (error) {
+    return next(error);
+  }
+}
+
+module.exports = {
+  createPayment,
+  checkoutPayment,
+  checkStatus,
+  cancelPayment,
+  paymentHistory,
+};
